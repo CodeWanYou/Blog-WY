348963561c0cf17af9600457dfd45a77b12a4721
CodeWanYou <135234141+CodeWanYou@users.noreply.github.com>
2023-11-05 16:20:23 +0000

    Deploying to gh-pages from @ CodeWanYou/Blog-WY@4b2b521b8fdb7d36951e7f1df4b3d2224243b9b3 🚀

---
+++ assets/vidstack-audio-1c139d7b.js
@@ -0,0 +1,1 @@
+import{H as t}from"./vidstack-db4cc3e9-0bd1f5d3.js";import"./vidstack-2fd2b44b-388e26c8.js";import"./app-afdc9e65.js";class o extends t{constructor(){super(...arguments),this.$$PROVIDER_TYPE="AUDIO"}get type(){return"audio"}setup(e){super.setup(e),this.type==="audio"&&e.delegate.t("provider-setup",{detail:this})}get audio(){return this.u}}export{o as AudioProvider};
