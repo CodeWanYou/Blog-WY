accaefbc7d67498f920e5895fcec8697d5210694
CodeWanYou <135234141+CodeWanYou@users.noreply.github.com>
2024-02-22 12:15:53 +0000

    Deploying to gh-pages from @ CodeWanYou/Blog-WY@4ff3908834b3f85747ff2646c223c343cf8ca8d8 🚀

---
+++ assets/pageview-e4769e12.js
@@ -0,0 +1,1 @@
+import{S as e}from"./app-068f8810.js";const a=()=>e({serverURL:"https://waline-comment.vuejs.press"});export{a as updatePageview};
---
+++ assets/vidstack-audio-851bb95e.js
@@ -0,0 +1,1 @@
+import{H as t}from"./vidstack-db4cc3e9-27dacf68.js";import"./vidstack-2fd2b44b-1bdc42e8.js";import"./app-068f8810.js";class o extends t{constructor(){super(...arguments),this.$$PROVIDER_TYPE="AUDIO"}get type(){return"audio"}setup(e){super.setup(e),this.type==="audio"&&e.delegate.t("provider-setup",{detail:this})}get audio(){return this.u}}export{o as AudioProvider};
